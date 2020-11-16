dd4812f075425906e61cad351fb3d2a5884127b1
Rebecca Weiss <rebeccascottweiss@gmail.com>
2020-11-16 14:47:15 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'; 
+import Tree from "./components/Tree";
+import AddChild from "./views/AddChild"; 
 import './App.css';
+import { Router } from "@reach/router"; 
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <Tree path="/" />
+          <AddChild path="/newChild" />
+        </Router>
     </div>
   );
 }
