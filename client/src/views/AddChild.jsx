0000c0a2214f4ef6c222c3002079ee6fe179224e
Rebecca Weiss <rebeccascottweiss@gmail.com>
2020-11-16 14:47:45 -0800

    Create AddChild.jsx

---
+++ client/src/views/AddChild.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Link, navigate } from "@reach/router"; 
+// import axios from "axios"; 
+import ChildForm from "../components/ChildForm"; 
+
+const New = (props) => {
+    const [name, setName] = useState(""); 
+    const [errors, setErrors] = useState([]); 
+
+    const createChild = (newChild) =>{
+        console.log(`We are adding this child to the database: ${newChild}`)
+    //   axios
+    //         .post("http://localhost:8000/api/authors/new", newChild)
+    //         .then((res)=>{
+    //             console.log(res.data); 
+    //             navigate("/"); 
+    //         })
+    //         .catch((err)=>{
+    //           const errorResponse = err.response.data.errors; // Get the errors from err.response.data
+    //           const errorArr = []; // Define a temp error array to push the messages in
+    //           for (const key of Object.keys(errorResponse)) { // Loop through all errors and get the messages
+    //               errorArr.push(errorResponse[key].message)
+    //           }
+    //           // Set Errors
+    //           setErrors(errorArr);
+    //         });
+    }
+
+  return <div>
+    <Link to="/">Home</Link>
+    <p style={{color:"purple"}}>Add a new author:</p>
+    <ChildForm onSubmitProp = { createChild } name={ name } setName={ setName } errors={ errors } setErrors={ setErrors } />
+  </div>;
+};
+
+export default New;
