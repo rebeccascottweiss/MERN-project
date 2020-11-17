1ca0643db7f043681a815a44059685cb380e277d
brianv-17 <69707282+brianv-17@users.noreply.github.com>
2020-11-17 11:30:35 -0800

    Update Donation.jsx

--- client/src/components/Donation.jsx
+++ client/src/components/Donation.jsx
@@ -3,13 +3,13 @@ import axios from "axios"
 
 const Donation = () => {
     const [donation, setDonation] = useState(0)
-    const [ name, setName] = useState("")
-    const [ recipient, setRecipient] = useState("")
+    const [anonname, setAnonName] = useState("")
+    const [recipient, setRecipient] = useState("")
     const submitHandler =(e)=>{
         e.preventDefault();
         axios.post("http://localhost:8000/api/",{
             donation, 
-            name,
+            anonname,
             recipient,
         })
         .then(res=>console.log(res.data))
@@ -21,7 +21,7 @@ const Donation = () => {
             <form onSubmit={submitHandler}>
                 <p>Donation Amount: <input type= "Number"  onChange={e=>setDonation(e.target.value)}/></p>
                 <p>Recipient: <input type="Text" placeholder="Leave Blank If General" onChange={e=>setRecipient(e.target.value)}/></p>
-                <p>Name: <input type="Text" placeholder="Leave Blank If Anonymous" onChange={e=>setName(e.target.value)}/></p>
+                <p>Name: <input type="Text" placeholder="Leave Blank If Anonymous" onChange={e=>setAnonName(e.target.value)}/></p>
                 <button>Be their Change</button>
             </form>
         </div>
