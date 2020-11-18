6b2bad824fd632c92db173159d76870b2db80367
Donna-N <jdranolan@gmail.com>
2020-11-18 15:22:09 -0700

    Update Donation.jsx

--- client/src/components/Donation.jsx
+++ client/src/components/Donation.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from "react"
 import axios from "axios"
 import { navigate } from "@reach/router"
+import Header from "./Header"
 
 const Donation = () => {
     const [donation, setDonation] = useState(0)
@@ -18,14 +19,28 @@ const Donation = () => {
     }
     return(
         <div> 
-            <h2>Make Donation to Organization</h2>
-            <form onSubmit={submitHandler}>
-                <p>Donation Amount: <input type= "Number" step="0.01" onChange={e=>setDonation(e.target.value)}/></p>
-                <p>Recipient: <input type="Text" placeholder="Leave Blank If General" onChange={e=>setRecipient(e.target.value)}/></p>
-                <p>Name: <input type="Text" placeholder="Leave Blank If Anonymous" onChange={e=>setAnonName(e.target.value)}/></p>
-                <button>Be their Change</button>
-            </form>
+            <Header />
+            <div style = {{color: "#F9E2B6"}}>
+                <br/>
+                <h2>Make Donation to Organization</h2>
+                <br/>
+                <form onSubmit={submitHandler}>
+                    <div className = "form-group row d-flex justify-content-center">
+                        <label for ="donation" className="col-sm-2 col-form-label"> Donation Amount: $</label>
+                        <input type= "Number" step="0.01" className="form-control col-sm-6" style = {{backgroundColor: "#F9E2B6", border: "#F9E2B6"}} onChange={e=>setDonation(e.target.value)}/>
+                    </div>
+                    <div className = "form-group row d-flex justify-content-center">
+                        <label for = "recipient" className="col-sm-2 col-form-label">Recipient:</label> 
+                        <input type="Text" className = "form-control col-sm-6" placeholder="Leave Blank If General" style = {{backgroundColor: "#F9E2B6", border: "#F9E2B6"}} onChange={e=>setRecipient(e.target.value)}/>
+                    </div>
+                    <div className = "form-group row d-flex justify-content-center">
+                        <label for = "name" className="col-sm-2 col-form-label">Name: </label> 
+                        <input type="Text" className = "form-control col-sm-6" placeholder="Leave Blank If Anonymous" style = {{backgroundColor: "#F9E2B6", border: "#F9E2B6"}} onChange={e=>setAnonName(e.target.value)}/>
+                    </div>
+                    <button type = "button" className = "btn-sm" style = {{backgroundColor: "#606F4E", color: "#F9E2B6"}}>Be their Change</button>
+                </form>
+            </div>
         </div>
     )
- }
+}
 export default Donation
